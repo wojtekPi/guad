8d4431ecced6191be3157fdd71b5c28b64b6e119
Wojtek Pietrucha <wojtekpietrucha@gmail.com>
2017-10-28 19:51:46 +0200

    Initial commit

---
+++ src/main/java/com/wojtek/pi/guad/GuadApplication.java
@@ -0,0 +1,14 @@
+package com.wojtek.pi.guad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuadApplication.class, args);
+	}
+
+
+}
---
+++ src/test/java/com/wojtek/pi/guad/GuadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wojtek.pi.guad;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GuadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
